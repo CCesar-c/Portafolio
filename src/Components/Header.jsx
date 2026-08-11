bfe738f1d179f0631b59ee39a74e0ace5c1090fb
CCesar-c <cesarinfluencer14@gmail.com>
2026-08-11 16:21:05 -0300

    fixed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,12 @@
 import rotas from "../Rutas/rotas.jsx";
+import "../Styles/style.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div id="header">
       {rotas.map((rt) => {
-        return <button key={rt.id}>{rt.name}</button>;
+        return <Link key={rt.id} to={rt.path}>{rt.name}</Link>
       })}
     </div>
   );
--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -5,8 +5,8 @@ export default function Index() {
   return (
     <div id="body">
       <Header />
-      <div style={{ backgroundColor: "blue" }}></div>
-      <div style={{ backgroundColor: "red" }}></div>
+      <div style={{ backgroundColor: "blue", height: "50vh", width: "100vw" }}></div>
+      <div style={{ backgroundColor: "red", height: "50vh", width: "100vw" }}></div>
     </div>
   );
 }
