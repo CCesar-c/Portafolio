146d62bbe602fca8065da54157094a96811d6299
CCesar-c <cesarinfluencer14@gmail.com>
2026-08-23 17:23:48 -0300

    algo

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -7,8 +7,16 @@ export default function Index() {
     <div id="body">
       <Header />
       <div id="primary">
-        <div id="card"></div>
-        <div id="card"></div>
+        <div id="card">
+          <h3>
+            Ola, seja bem-vindo ao meu portifolio... O meu nome e cesar gosta de
+            aprender programação e novas tecnicas de fazer mais optimzas e
+            melhores com mais fluidez.
+          </h3>
+        </div>
+        <div id="card">
+          <img src="" />
+        </div>
       </div>
       <Divisor />
       <div id="second"></div>
