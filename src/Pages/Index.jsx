9b8e9cd264285355702fac03c64ed8db271b6f18
CCesar-c <lianbastiam@gmail.com>
2026-09-11 21:46:05 -0300

    enter commit message

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -11,23 +11,55 @@ export default function Index() {
       <Header />
       <div id="primary">
         <div id="card">
-            <h2>Ola, seja bem-vindo ao meu portifolio...</h2>
+          <h2>Ola, seja bem-vindo ao meu portifolio...</h2>
           <h3>
-            O meu nome e cesar gosto de aprender programação e novas tecnicas de fazer mais
-            optimzas e melhores com mais fluidez.
+            O meu nome e cesar gosto de aprender programação e novas tecnicas de
+            fazer mais optimzas e melhores com mais fluidez.
           </h3>
         </div>
         <div id="card">
-          <img src={foto.toString()} height={"100%"} width={"100%"} />
+          <img src={foto.toString()} height={"10%"} width={"10%"} />
         </div>
       </div>
       <Divisor />
+
+      <h2>Com o que eu trabalho?</h2>
+
       <div id="second">
-        <h2>Linguagem que domino e uso</h2>
-        <div className="content-languages" >
-          {All[0].Linguagens.map((ln, i) => {
-            return <Globo key={i} nome={ln.name} />;
-          })}
+        <div id="content">
+          <h3>Linguagens</h3>
+          <div className="content-languages">
+            {All[0].Linguagens.map((ln, i) => {
+              return <Globo key={i} nome={ln.name} />;
+            })}
+          </div>
+        </div>
+
+        <div id="content">
+          <h3>FrameWorks</h3>
+          <div className="content-languages">
+            {All[0].Framework.map((ln, i) => {
+              return <Globo key={i} nome={ln.name} />;
+            })}
+          </div>
+        </div>
+
+        <div id="content">
+          <h3>Bibliotecas</h3>
+          <div className="content-languages">
+            {All[0].Bibliotecas.map((ln, i) => {
+              return <Globo key={i} nome={ln.name} />;
+            })}
+          </div>
+        </div>
+
+        <div id="content" >
+          <h3>Ferramentas</h3>
+          <div className="content-languages">
+            {All[0].Ferramentas.map((ln, i) => {
+              return <Globo key={i} nome={ln.name} />;
+            })}
+          </div>
         </div>
       </div>
       <Footer />
