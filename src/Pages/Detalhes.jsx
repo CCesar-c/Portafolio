9f8a19a228be4d1410ae1f395a7bd6aa26820ddc
CCesar-c <cesarinfluencer14@gmail.com>
2026-09-24 13:49:59 -0300

    sim

--- src/Pages/Detalhes.jsx
+++ src/Pages/Detalhes.jsx
@@ -1,13 +1,16 @@
 import "../Styles/style.css";
 import Header from "../Components/Header.jsx";
 import Footer from "../Components/Footer.jsx";
+import { useParams } from "react-router";
 
 export default function Detalhes() {
-  // const {id} =
+  const { id } = useParams();
   return (
     <div id="body">
       <Header />
-      <main className="page"></main>
+      <main className="page">
+        <img src={} height={100} width={100} />
+      </main>
       <Footer />
     </div>
   );
